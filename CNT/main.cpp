d82dd4c87e9696220aa4cc7aafa1c460ab304a3f
Jens Brörken <jens.broerken@gmail.com>
2019-10-25 12:28:06 +0200

    Initial Commit

---
+++ CNT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CNT
+//
+//  Created by Jens Brörken on 25.10.19.
+//  Copyright © 2019 Jens Brörken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
